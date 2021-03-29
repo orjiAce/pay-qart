24f4ac5cd21a41f53cea332e7b499a6ecf6ddb00
Orji Joseph <orjiace@gmail.com>
2021-03-29 18:31:27 +0100

    first commits

--- component/Plan.js
+++ component/Plan.js
@@ -11,32 +11,32 @@ const plans =
     [
         {
             name: "Aggresive",
-            number:"1"
+            number: "1"
         },
         {
             name: "Stretching",
-           number:"2"
+            number: "2"
         },
         {
             name: "Focused",
             number: "3"
         }, {
-            name: "Casual",
-            number: "4"
-        }, {
-            name: "Mild",
-            number: "5"
-        }, {
-            name: "Gentle",
-            number: "6"
-        },
+        name: "Casual",
+        number: "4"
+    }, {
+        name: "Mild",
+        number: "5"
+    }, {
+        name: "Gentle",
+        number: "6"
+    },
 
     ];
 
 
 const YourPlan = (props) => {
 
-const {updateBreakDown} = props
+    const {updateBreakDown} = props
 
 
     const [monthPlan, setMonthPlan] = useState('1');
@@ -56,7 +56,7 @@ const {updateBreakDown} = props
         const interest = (shoppingCredit * monthPlan) / 100
 
         //
-        const totalInterestPayable = interest * monthPlan ;
+        const totalInterestPayable = interest * monthPlan;
         const monthlyRepayable = (shoppingCredit + totalInterestPayable) / monthPlan
         console.log(monthlyRepayable)
         setMonthPay(monthlyRepayable)
@@ -72,93 +72,89 @@ const {updateBreakDown} = props
     const shoppingCredit = cartValue - miniMumPayment;
     const interest = (shoppingCredit * 4) / 100
 
-const updatePlan = () =>{
+    const updatePlan = () => {
         const data = new FormData()
-    data.append("cartValue", cartValue)
-    data.append("monthPlan", monthPlan)
-    updateBreakDown(data)
-}
+        data.append("cartValue", cartValue)
+        data.append("monthPlan", monthPlan)
+        updateBreakDown(data)
+    }
 
     return (
         <div className={styles.stageOneWrap}>
             <div className={styles.title}>
-             Choose your plan
+                Choose your plan
 
             </div>
-<div className="formWrap" >
-    <RadioGroup containerStyle="planContainer" onChange={onChange}>
-                {plans.map((({name, number}, index) => (
-                    <Radio
+            <div className="formWrap">
+                <RadioGroup containerStyle="planContainer" onChange={onChange}>
+                    {plans.map((({name, number}, index) => (
+                        <Radio
 
-                        key={index}
-                        className='radio'
-                        value={number}
-                        render={({isSelected}) => (
-                            <div
-                                className="plans"
-                            >
-                                <div className='roof'>
+                            key={index}
+                            className='radio'
+                            value={number}
+                            render={({isSelected}) => (
+                                <div
+                                    className="plans"
+                                >
+                                    <div className='roof'>
 
-                                </div>
-                                <div className='wrap'
-                                     style={{
-                                         backgroundColor: ` ${isSelected ? "#3a0162" : "#ffffff"} `,
-                                         color: ` ${isSelected ? "#ffffff" : "#131313"} `
-                                     }}>
-                                    <div className='planTitle'>
-                                        {name}
+                                    </div>
+                                    <div className='wrap'
+                                         style={{
+                                             backgroundColor: ` ${isSelected ? "#3a0162" : "#ffffff"} `,
+                                             color: ` ${isSelected ? "#ffffff" : "#131313"} `
+                                         }}>
+                                        <div className='planTitle'>
+                                            {name}
+                                        </div>
+
+                                        <div className='number'>
+                                            {number}
+                                        </div>
+
+                                        <div className='month'>Month</div>
                                     </div>
 
-                                    <div className='number'>
-                                        {number}
-                                    </div>
-
-                                    <div className='month'>Month</div>
                                 </div>
+                            )}
+                        />
+                    )))}
+                </RadioGroup>
 
-                            </div>
-                        )}
-                    />
-                )))}
-            </RadioGroup>
+                <div className='planTitle'>
+                    Payment breakdown
+                </div>
 
-    <div className='planTitle'>
-        Payment breakdown
-    </div>
+                <div className='breakDownBox'>
+                    <div>
+                        <ul>
+                            <li>Shopping credit</li>
+                            <li>Down payment</li>
+                            <li>Monthly installment</li>
+                            <li>Tenure</li>
+                        </ul>
+                    </div>
+                    <div className='numbers'>
+                        <ul>
+                            <li>{shoppingCredit}</li>
+                            <li>{miniMumPayment}</li>
+                            <li>{pay}</li>
+                            <li>{monthPlan}</li>
+                        </ul>
+                    </div>
 
-    <div className='breakDownBox'>
-<div>
-   <ul>
-       <li>Shopping credit</li>
-       <li>Down payment</li>
-       <li>Monthly installment</li>
-       <li>Tenure</li>
-   </ul>
-</div>
-        <div className='numbers'>
-   <ul>
-       <li>{shoppingCredit}</li>
-       <li>{miniMumPayment}</li>
-       <li>{pay}</li>
-       <li>{monthPlan}</li>
-   </ul>
-</div>
+                    <div>
 
-        <div>
-
-        </div>
+                    </div>
 
 
-    </div>
-    <button className='continueBtn' onClick={updatePlan}>
-        Continue
-    </button>
+                </div>
+                <button className='continueBtn' onClick={updatePlan}>
+                    Continue
+                </button>
 
-</div>
-
-
-
-
+            </div>
 
 
         </div>
@@ -173,7 +169,7 @@ const mapActionsToDispatch = {
     updateBreakDown
 }
 
-const mapStateToProps = (state) =>({
+const mapStateToProps = (state) => ({
     data: state.data
 })
 
